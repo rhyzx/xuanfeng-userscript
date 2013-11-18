f673779628717dab9352926844078e47e99a5f3d
U0 <rhyzix@gmail.com>
2013-11-18 11:03:05 +0800

    release 0.6.1

--- xuanfeng.user.js
+++ xuanfeng.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name       XuanFengEx
 // @namespace  https://github.com/rhyzx/xuanfeng-userscript
-// @version    0.6.0
+// @version    0.6.1
 // @description  QQ旋风网页版离线下载增强
 // @match      http://lixian.qq.com/main.html*
 // @copyright  2013+, rhyzx
